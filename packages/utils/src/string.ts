efe3a740a1d1e3da48c563a3b3bc51e94d7bf0da
Andrew Dibble <andrew@chainflip.io>
2024-05-21 22:35:12 +0200

    feat(util): add split util

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -1,3 +1,10 @@
 export const toUpperCase = <const T extends string>(str: T) => str.toUpperCase() as Uppercase<T>;
 
 export const toLowerCase = <const T extends string>(str: T) => str.toLowerCase() as Lowercase<T>;
+
+type Split<T extends string, D extends string> = T extends `${infer L}${D}${infer R}`
+  ? [L, ...Split<R, D>]
+  : [T];
+
+export const split = <const T extends string, D extends string>(str: T, delimiter: D) =>
+  str.split(delimiter) as Split<T, D>;
