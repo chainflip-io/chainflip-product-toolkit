63663e1423bf1d23dd6f23731b68be2872d1a33a
Andrew Dibble <andrew@chainflip.io>
2024-05-24 23:12:36 +0200

    chore: add commit hooks

---
+++ lint-staged.config.js
@@ -0,0 +1,33 @@
+import micromatch from 'micromatch';
+import * as path from 'path';
+
+/**
+ * @param {string[]} files
+ * @returns {string[]}
+ */
+export default function lint(files) {
+  const commands = [];
+
+  const packages = micromatch(files, '**/packages/**/*')
+    .map((file) =>
+      file
+        .replace(import.meta.dirname, '')
+        .match(new RegExp(`packages${path.sep}([^${path.sep}]+)`))
+        ?.pop(),
+    )
+    .filter(Boolean)
+    .map((pkg) => `pnpm --filter ${pkg} test run`);
+
+  commands.push(...packages);
+
+  const tsFiles = micromatch(files, '**/*.ts(x)?').map((file) =>
+    file.replace(import.meta.dirname, '').slice(1),
+  );
+
+  if (tsFiles.length) {
+    commands.push(`pnpm prettier --check ${tsFiles.join(' ')}`);
+    commands.push(`pnpm eslint --max-warnings 0 ${tsFiles.join(' ')}`);
+  }
+
+  return commands;
+}
