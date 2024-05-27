0d1b3c2c8b8eb2fff9ae3999d6452ebb19aaf6e4
Andrew Dibble <andrew@chainflip.io>
2024-05-27 15:26:36 +0200

    chore(rpc): update tsup config

--- packages/rpc/tsup.config.mjs
+++ packages/rpc/tsup.config.mjs
@@ -1,11 +1,22 @@
 import { defineConfig } from 'tsup';
+import { esbuildPluginFilePathExtensions } from 'esbuild-plugin-file-path-extensions';
 
 export default defineConfig({
   splitting: true,
   minify: false,
   dts: true,
-  bundle: false,
+  bundle: true,
   format: ['cjs', 'esm'],
   entry: ['./src/*.ts'],
   target: 'es2022',
+  outExtension: ({ format }) => ({
+    js: format === 'esm' ? '.mjs' : '.cjs',
+    dts: format === 'esm' ? '.d.mts' : '.d.cts',
+  }),
+  esbuildPlugins: [
+    esbuildPluginFilePathExtensions({
+      cjsExtension: 'cjs',
+      esmExtension: 'mjs',
+    }),
+  ],
 });
