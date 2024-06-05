27e9f4b282fd0c0725a623c8dd78db782c860a17
Andrew Dibble <andrew@dibble.codes>
2024-06-05 18:48:44 +0200

    refactor(processor): simplify and test codegen

--- packages/processor/scripts/generate.ts
+++ packages/processor/scripts/generate.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env -S pnpm tsx
+import * as fs from 'fs/promises';
+import * as path from 'path';
 import Parser, { MetadataOpts } from '../src/codegen/Parser';
 import CodeGenerator from '../src/codegen/CodeGenerator';
 import { diffSpecs, specVersionCache } from '../src/codegen/utils';
+
+const generatedDir = path.join(import.meta.dirname, '..', 'generated');
 
 const generateAllCode = async () => {
   const info = await specVersionCache.read();
@@ -15,9 +19,13 @@ const generateAllCode = async () => {
   let previousMetadata = {};
 
   for (const { metadata, specVersion } of metadataForHashes) {
+    const specDir = path.join(generatedDir, `${specVersion}`);
+    await fs.rm(specDir, { recursive: true, force: true });
     const eventsChanged = diffSpecs(previousMetadata, metadata);
     const generator = new CodeGenerator({ trackedEvents: eventsChanged });
-    await generator.generate(specVersion, metadata);
+    for (const module of generator.generate(metadata)) {
+      await module.writeFile(specDir);
+    }
     previousMetadata = metadata;
   }
 };
