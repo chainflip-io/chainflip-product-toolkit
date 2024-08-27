f2b162bdfb5febdd784948ddd8db4c8798ff2d30
Andrew Dibble <andrew@chainflip.io>
2024-08-27 17:08:49 +0200

    chore(rpc): update url

--- packages/rpc/src/constants.ts
+++ packages/rpc/src/constants.ts
@@ -1,5 +1,5 @@
 export const PUBLIC_RPC_ENDPOINTS = {
-  mainnet: 'https://mainnet-archive.chainflip.io',
+  mainnet: 'https://rpc.mainnet.chainflip.io',
   perseverance: 'https://archive.perseverance.chainflip.io',
   sisyphos: 'https://archive.sisyphos.chainflip.io',
   backspin: 'https://backspin-rpc.staging',
