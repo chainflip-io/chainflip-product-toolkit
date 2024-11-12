803e78ae32990f27221f8714c76813219ffad79e
David Cumps <david@cumps.be>
2024-11-12 11:49:57 +0100

    feat: add instaswap broker

--- packages/utils/src/consts.ts
+++ packages/utils/src/consts.ts
@@ -64,6 +64,7 @@ export const brokerAliasMap: AccountAliasMap = {
     name: 'SubWallet',
     twitter: '@subwalletapp',
   },
+  cFMjbSKgEiUacx4pqmSMVajksRf2Vhvefm4Lc5dwJxKA6DW3E: { name: 'InstaSwap', twitter: '@instaswap_io' },
 } as const;
 
 export const lpAliasMap: AccountAliasMap = {
