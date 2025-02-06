e7fef32994e3d10f226ad02aa517034a9d97d7fb
Andrew Dibble <andrew@chainflip.io>
2025-02-06 14:24:38 +0100

    refactor(utils): add bytelike (#213)

--- packages/utils/src/base58.ts
+++ packages/utils/src/base58.ts
@@ -1,8 +1,8 @@
 import { decodeBytesWithCharset, encodeBytesWithCharset } from './bytes';
+import { type Bytelike } from './types';
 
 export const CHARSET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
 
-export const encode = (bytes: Uint8Array | number[]): string =>
-  encodeBytesWithCharset(bytes, CHARSET);
+export const encode = (bytes: Bytelike): string => encodeBytesWithCharset(bytes, CHARSET);
 
 export const decode = (input: string): Uint8Array => decodeBytesWithCharset(input, CHARSET);
--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -1,1 +1,3 @@
 export type HexString = `0x${string}`;
+
+export type Bytelike = Uint8Array | number[] | HexString;
