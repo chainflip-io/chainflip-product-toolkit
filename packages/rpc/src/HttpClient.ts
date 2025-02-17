6a7626f6abbb0c72af7ec6bf1f4e6dab0c7db006
Andrew Dibble <andrew@chainflip.io>
2025-02-17 16:58:26 +0100

    fix(rpc): handle network errors (#243)

--- packages/rpc/src/HttpClient.ts
+++ packages/rpc/src/HttpClient.ts
@@ -24,13 +24,22 @@ export default class HttpClient extends Client {
   protected async send<const T extends RpcMethod>(
     request: JsonRpcRequest<T>[],
   ): Promise<Response[]> {
-    const res = await fetch(this.url, {
-      body: JSON.stringify(request),
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    let res;
+    try {
+      res = await fetch(this.url, {
+        body: JSON.stringify(request),
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+    } catch (error) {
+      return request.map((r) => ({
+        id: r.id,
+        success: false,
+        error: new Error('Network error', { cause: error }),
+      }));
+    }
 
     if (!res.ok) {
       return request.map((r) => ({
