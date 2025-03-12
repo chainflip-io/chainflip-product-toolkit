8e0f53efe41c7c05d32a8ae223f2122be4b1b75b
Andrew Dibble <andrew@chainflip.io>
2025-03-12 16:18:18 +0100

    Update packages/utils/src/types.ts
    Co-authored-by: Niklas Natter <niklas.natter@gmail.com>

--- packages/utils/src/types.ts
+++ packages/utils/src/types.ts
@@ -4,6 +4,7 @@ export type HexString = `0x${string}`;
 
 export type Bytelike = Uint8Array | number[] | HexString;
 
+// https://github.com/chainflip-io/chainflip-backend/blob/f9b281c1fa4862da0e8a9007214afa96de5eb6f1/api/bin/chainflip-ingress-egress-tracker/src/witnessing/state_chain.rs#L139
 export type VaultSwapData<T = never> = {
   amount: bigint;
   destinationAddress: string;
