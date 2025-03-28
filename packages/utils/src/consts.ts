8b9c830da81857cb8b3bf32a29fac3d55a873dbc
Sukhovii Anastasiia <anastasia@chainflip.io>
2025-03-28 17:43:07 +0100

    added symbiosis

--- packages/utils/src/consts.ts
+++ packages/utils/src/consts.ts
@@ -29,6 +29,7 @@ type AccountSmartContractAliasMap = {
 
 export const smartContractAliasMap: AccountSmartContractAliasMap = {
   '0xce16F69375520ab01377ce7B88f5BA8C48F8D666': 'Squid',
+  '0xd99ac0681b904991169a4f398b9043781adbe0c3': 'Symbiosis',
 };
 
 export const brokerAliasMap: AccountAliasMap = {
