2c5c2e3234ff8b411599745785c3f79a9392830b
Andrew Dibble <andrew@chainflip.io>
2025-04-14 15:38:38 +0200

    feat(processor): log latest versions

--- packages/processor/scripts/generate.ts
+++ packages/processor/scripts/generate.ts
@@ -1,9 +1,34 @@
 #!/usr/bin/env -S pnpm tsx
+import { chainflipChains } from '@chainflip/utils/chainflip';
 import * as path from 'path';
 import generateAllCode from '@/chainspec/generateAllCode';
 import CodeGenerator from '../src/codegen/CodeGenerator';
 import Parser from '../src/codegen/Parser';
+import trackLatestVersions from '../src/codegen/trackLatestVersions';
 
 const generatedDir = path.join(import.meta.dirname, '..', 'generated');
 
-await generateAllCode(Parser, CodeGenerator, generatedDir);
+const latestVersions = await generateAllCode(
+  Parser,
+  CodeGenerator,
+  generatedDir,
+  trackLatestVersions,
+);
+
+const chainRegExp = new RegExp(`^(${chainflipChains.join('|')})(.+)$`, 'i');
+
+for (const msg of latestVersions
+  .entries()
+  .flatMap(([pallet, events]) => {
+    const [, chain = '', palletName = pallet] = pallet.match(chainRegExp) ?? [];
+
+    return events
+      .entries()
+      .map(([event, version]) =>
+        `${palletName}.${event}: ${version} ${chain && `(${chain})`}`.trim(),
+      );
+  })
+  .toArray()
+  .sort()) {
+  console.log(msg);
+}
