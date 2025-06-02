dc538267ae2d623ba82e1ecbadc4e56710ecdf8a
David Cumps <david@cumps.be>
2025-06-02 11:45:40 +0200

    feat: add dexifier broker id

--- packages/utils/src/consts.ts
+++ packages/utils/src/consts.ts
@@ -95,10 +95,6 @@ export const brokerAliasMap: AccountAliasMap = {
     name: 'SwapperXYZ',
     twitter: null,
   },
-  cFNddmecxrTKfytLwV3s7KpBjmsvFAX8xUnrXfnvd4yqtr4vC: {
-    name: 'Dexifier',
-    twitter: '@DexifierX',
-  },
   cFNwtr2mPhpUEB5AyJq38DqMKMkSdzaL9548hajN2DRTwh7Mq: {
     name: 'SwapKit',
     twitter: '@SwapKitPowered',
@@ -123,7 +119,15 @@ export const brokerAliasMap: AccountAliasMap = {
     name: 'Trust Wallet',
     twitter: '@TrustWallet',
   },
+  cFNddmecxrTKfytLwV3s7KpBjmsvFAX8xUnrXfnvd4yqtr4vC: {
+    name: 'Dexifier',
+    twitter: '@DexifierX',
+  },
   cFKCm1mNBmVH2Lwyp2NTzPa1JNs4KYt19pX3yjbTy7GdCr3aB: {
+    name: 'Dexifier',
+    twitter: '@DexifierX',
+  },
+  cFN2adJMj49HPc8kUbrMunf7SSmrg6v77kTgZXgGFjtS9YyGB: {
     name: 'Dexifier',
     twitter: '@DexifierX',
   },
