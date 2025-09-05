93e81f3309cc85a9882bcc7a2890d0692a909144
Andrew Dibble <andrew@chainflip.io>
2025-09-05 14:29:41 +0200

    refactor(delegation): add new API

--- packages/delegation/src/scale.ts
+++ packages/delegation/src/scale.ts
@@ -3,13 +3,18 @@ import { bytesToHex, hexToBytes } from '@chainflip/utils/bytes';
 import * as ss58 from '@chainflip/utils/ss58';
 import { _void, Bytes, CodecType, Enum, Option, Struct, u128 } from 'scale-ts';
 
+const DelegationAmount = Enum({
+  Max: _void,
+  Some: u128,
+});
+
 const DelegationApi = Enum({
   Delegate: Struct({
     operator: Bytes(32),
+    increase: DelegationAmount,
   }),
-  Undelegate: _void,
-  SetMaxBid: Struct({
-    maybeMaxBid: Option(u128),
+  Undelegate: Struct({
+    decrease: DelegationAmount,
   }),
   Redeem: Struct({
     amount: Enum({ Max: _void, Exact: u128 }),
@@ -23,13 +28,8 @@ export const EthereumSCApi = Enum({
 });
 
 export type CallData =
-  | { call: 'delegate'; operator: `cF${string}` }
-  | { call: 'undelegate' }
-  | {
-      call: 'setMaxBid';
-      /** The new max bid, if set to `undefined`, it will be */
-      maxBid?: bigint | undefined;
-    }
+  | { call: 'delegate'; operator: `cF${string}`; increaseBy: bigint | undefined }
+  | { call: 'undelegate'; decreaseBy: bigint | undefined }
   | {
       call: 'redeem';
       /** The amount to redeem, if `undefined`, all available FLIP will be redeemed */
@@ -37,17 +37,24 @@ export type CallData =
       address: `0x${string}`;
     };
 
+const buildDelegationAmount = (amount: bigint | undefined) =>
+  amount ? ({ tag: 'Some', value: amount } as const) : ({ tag: 'Max', value: undefined } as const);
+
 const buildDelegationApiData = (data: CallData): CodecType<typeof DelegationApi> => {
   switch (data.call) {
     case 'delegate':
       return {
         tag: 'Delegate',
-        value: { operator: ss58.decode(data.operator).data },
+        value: {
+          operator: ss58.decode(data.operator).data,
+          increase: buildDelegationAmount(data.increaseBy),
+        },
       } as const;
     case 'undelegate':
-      return { tag: 'Undelegate', value: undefined } as const;
-    case 'setMaxBid':
-      return { tag: 'SetMaxBid', value: { maybeMaxBid: data.maxBid } } as const;
+      return {
+        tag: 'Undelegate',
+        value: { decrease: buildDelegationAmount(data.decreaseBy) },
+      } as const;
     case 'redeem':
       return {
         tag: 'Redeem',
