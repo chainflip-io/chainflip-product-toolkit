64c3f8a8857035ae4430d10a607b796ca9b6509b
Andrew Dibble <andrew@chainflip.io>
2026-01-28 14:27:46 +0100

    tsdown

--- packages/utils/tsdown.config.mjs
+++ packages/utils/tsdown.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   minify: false,
   dts: true,
-  bundle: true,
+  unbundle: true,
   format: ['cjs', 'esm'],
   external: ['*'],
   noExternal: ['@noble/hashes/*'],
   entry: ['./src/*.ts'],
   target: 'es2022',
+  outExtensions: ({ format }) => ({
+    js: format === 'es' ? '.js' : '.cjs',
+    dts: '.d.ts',
+  }),
 });
