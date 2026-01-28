1671a2808d561fc622dd472593c47bec3568cc25
Andrew Dibble <andrew@chainflip.io>
2026-01-28 14:42:58 +0100

    tsdown

---
+++ packages/scale/tsdown.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  minify: false,
+  dts: true,
+  bundle: true,
+  format: ['cjs', 'esm'],
+  skipNodeModulesBundle: true,
+  entry: ['./src/*.ts'],
+  target: 'es2022',
+  outExtensions: ({ format }) => ({
+    dts: '.d.ts',
+    js: format === 'es' ? '.js' : '.cjs',
+  }),
+});
