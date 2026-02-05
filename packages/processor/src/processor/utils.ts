1b1f97d4f15208fa90e7828d7a60aa9548114062
Andrew Dibble <andrew@chainflip.io>
2026-02-05 16:44:32 +0100

    fix export

--- packages/processor/src/processor/utils.ts
+++ packages/processor/src/processor/utils.ts
@@ -1,3 +1,5 @@
+import type { Semver } from './types';
+
 export const generatePalletEventName = <
   const C extends readonly string[],
   const P extends string,
@@ -17,3 +19,16 @@ export const generatePalletEventName = <
       [K in E[number]]: `${Ch}${P}.${K}`;
     };
   };
+
+export const parseSemver = (specId: string): Semver => {
+  // the specId is in the format of "chainflip-node@<specId>"
+  const specNumber = Number.parseInt(specId.split('@')[1], 10);
+  if (Number.isNaN(specNumber)) throw new Error('Invalid specId');
+  const specStr = specNumber.toString();
+  const segmentLength = Math.ceil(specStr.length / 3);
+  const padded = specStr.padStart(segmentLength * 3, '0');
+  const major = padded.slice(0, segmentLength);
+  const minor = padded.slice(segmentLength, segmentLength * 2);
+  const patch = padded.slice(segmentLength * 2, segmentLength * 3);
+  return `${Number(major)}.${Number(minor)}.${Number(patch)}`;
+};
