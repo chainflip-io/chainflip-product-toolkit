8f4240d82a246ae79ca3c8448eae4509ad509376
legape <gabriel.buragev96@gmail.com>
2026-05-05 13:15:33 +0200

    chore: move block spec parsing utils to toolkit (#1062)
    * chore: move block spec parsing utils to toolkit
    
    * add tests

---
+++ packages/utils/src/__tests__/chainflip.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest';
+import { isBlockSpecLessThanVersion } from '../chainflip';
+
+describe(isBlockSpecLessThanVersion, () => {
+  it('correctly compares spec versions', () => {
+    expect(isBlockSpecLessThanVersion('2.0.0', 'chainflip@201')).toBe(false);
+    expect(isBlockSpecLessThanVersion('2.1.0', 'chainflip@201')).toBe(true);
+    expect(isBlockSpecLessThanVersion('2.2.0', 'chainflip@201')).toBe(true);
+
+    expect(isBlockSpecLessThanVersion('2.1.0', 'chainflip@210')).toBe(false);
+    expect(isBlockSpecLessThanVersion('2.2.0', 'chainflip@210')).toBe(true);
+
+    expect(isBlockSpecLessThanVersion('2.2.0', 'chainflip@220')).toBe(false);
+    expect(isBlockSpecLessThanVersion('3.0.0', 'chainflip@220')).toBe(true);
+
+    expect(isBlockSpecLessThanVersion('3.0.0', 'chainflip@21200')).toBe(true);
+  });
+});
